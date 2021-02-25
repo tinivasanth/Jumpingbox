cad6d75118b673b46e6f40280c3a558470e77c77
Tini Vasanth <tini.vasanth@gmail.com>
2021-02-25 20:21:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    /*surf2 = createSprite(295,580,200,30);
+    surf2.shapeColor = "green";
+
+    surf3 = createSprite(515,580,200,30);
+    surf3.shapeColor = "red";
+
+    surf4 = createSprite(740,580,220,30);
+    surf4.shapeColor = "yellow";*/
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    ball.velocityX = 4;
+    ball.velocityY = 9;
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+        
+    ball.bounceOff(edges);
+
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = rgb(0,0,255);
+        music.play();
+    }
+
+    
+
+    drawSprites();
+}
